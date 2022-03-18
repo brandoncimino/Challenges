54e3652ddab0ed3d8300a1b6947978a72f131fd2
Brandon Cimino <bcimino@cccis.com>
2022-03-18 18:54:07 -0500

    Made tests more flexible and also check the `SPOILERS` types

--- Challenges/Challenge_4/Tests.cs
+++ Challenges/Challenge_4/Tests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Reflection;
 
 using NUnit.Framework;
@@ -6,28 +7,40 @@ using NUnit.Framework;
 namespace Challenges.Challenge_4;
 
 public class Tests {
-    private BronzePickaxe Pick = new BronzePickaxe();
-    private RingOfDueling Ring = new RingOfDueling();
-    private GenieLamp     Lamp = new GenieLamp();
-
     [Test(Description = "Demonstrates that each item has the correct Value, HighAlch, and LowAlch variables.")]
-    public void Challenge4_Variables() {
+    [TestCase(typeof(BronzePickaxe),          typeof(RingOfDueling),          typeof(GenieLamp))]
+    [TestCase(typeof(SPOILERS.BronzePickaxe), typeof(SPOILERS.RingOfDueling), typeof(SPOILERS.GenieLamp))]
+    public void Challenge4_Variables(Type pickType, Type ringType, Type lampType) {
+        // 📎 The `!` says, "Don't worry about this variable maybe being null, I know it won't be."
+        // See: https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/null-forgiving
+        //
+        // It seems weird that `Activator.CreateInstance`, which calls the default constructor, would return `null`,
+        // but apparently it can when you try to activate a `Nullable<>` type like `Nullable<int>`
+        // (usually written as `int?`)
+        CheckItemValues(
+            Activator.CreateInstance(pickType)!,
+            Activator.CreateInstance(ringType)!,
+            Activator.CreateInstance(lampType)!
+        );
+    }
+
+    public static void CheckItemValues(object pick, object ring, object lamp) {
         Assert.Multiple(
             () => {
                 // Value
-                Assert.That(Pick.GetVariableValue("Value"), Is.EqualTo(1),    $"{Pick.GetType().Name} Value"); // 1, 0, 0
-                Assert.That(Ring.GetVariableValue("Value"), Is.EqualTo(1275), $"{Ring.GetType().Name} Value"); // 1275, 765, 510
-                Assert.That(Lamp.GetVariableValue("Value"), Is.EqualTo(200),  $"{Lamp.GetType().Name} Value"); // 200, 120, 80
+                Assert.That(pick.GetVariableValue("Value"), Is.EqualTo(1),    $"{pick.GetType().Name} Value"); // 1, 0, 0
+                Assert.That(ring.GetVariableValue("Value"), Is.EqualTo(1275), $"{ring.GetType().Name} Value"); // 1275, 765, 510
+                Assert.That(lamp.GetVariableValue("Value"), Is.EqualTo(200),  $"{lamp.GetType().Name} Value"); // 200, 120, 80
 
                 // High Alch
-                Assert.That(Pick.GetVariableValue("HighAlch"), Is.EqualTo(0),   $"{Pick.GetType().Name} HighAlch");
-                Assert.That(Ring.GetVariableValue("HighAlch"), Is.EqualTo(765), $"{Ring.GetType().Name} HighAlch");
-                Assert.That(Lamp.GetVariableValue("HighAlch"), Is.EqualTo(120), $"{Lamp.GetType().Name} HighAlch");
+                Assert.That(pick.GetVariableValue("HighAlch"), Is.EqualTo(0),   $"{pick.GetType().Name} HighAlch");
+                Assert.That(ring.GetVariableValue("HighAlch"), Is.EqualTo(765), $"{ring.GetType().Name} HighAlch");
+                Assert.That(lamp.GetVariableValue("HighAlch"), Is.EqualTo(120), $"{lamp.GetType().Name} HighAlch");
 
                 // Low Alch
-                Assert.That(Pick.GetVariableValue("LowAlch"), Is.EqualTo(0),   $"{Pick.GetType().Name} LowAlch");
-                Assert.That(Ring.GetVariableValue("LowAlch"), Is.EqualTo(510), $"{Ring.GetType().Name} LowAlch");
-                Assert.That(Lamp.GetVariableValue("LowAlch"), Is.EqualTo(80),  $"{Lamp.GetType().Name} LowAlch");
+                Assert.That(pick.GetVariableValue("LowAlch"), Is.EqualTo(0),   $"{pick.GetType().Name} LowAlch");
+                Assert.That(ring.GetVariableValue("LowAlch"), Is.EqualTo(510), $"{ring.GetType().Name} LowAlch");
+                Assert.That(lamp.GetVariableValue("LowAlch"), Is.EqualTo(80),  $"{lamp.GetType().Name} LowAlch");
             }
         );
     }
@@ -36,15 +49,32 @@ public class Tests {
         typeof(RingOfDueling),
         typeof(BronzePickaxe),
         typeof(GenieLamp),
+        typeof(SPOILERS.RingOfDueling),
+        typeof(SPOILERS.BronzePickaxe),
+        typeof(SPOILERS.GenieLamp),
     };
 
-    [Test]
+    [Test(Description = "Demonstrates that each type has 3 read-only Properties and 0 Fields.")]
     public void Challenge4_Turbo([ValueSource(nameof(ItemTypes))] Type itemType) {
-        Assert.That(itemType.GetRuntimeFields,     Has.Count.EqualTo(1));
-        Assert.That(itemType.GetRuntimeProperties, Has.Count.EqualTo(3).And.All.Property("CanWrite").False);
+        Assert.Multiple(
+            () => {
+                Assert.That(
+                    itemType.GetRuntimeFields,
+                    Is.Empty,
+                    $"{itemType} cannot have any fields!"
+                );
+                Assert.That(
+                    itemType.GetRuntimeProperties,
+                    Has.Length.EqualTo(3).And.All.Property("CanWrite").False,
+                    $"{itemType} must have exactly 3 read-only properties!"
+                );
 
-        foreach (var meth in itemType.GetRuntimeMethods()) {
-            Assert.That(meth.DeclaringType != itemType || meth.IsSpecialName);
-        }
+                var declaredMethods = itemType.GetRuntimeMethods()
+                                              .Where(it => it.DeclaringType == itemType)
+                                              .Where(it => it.IsSpecialName == false);
+
+                Assert.That(declaredMethods, Is.Empty, $"{itemType} cannot declare any methods!");
+            }
+        );
     }
 }
