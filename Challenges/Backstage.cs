d48600e24f2361450834a5238fe7394b584df31b
Brandon Cimino <bcimino@cccis.com>
2022-03-18 18:40:40 -0500

    Filled out backstage methods & made the single-entry methods return `private` and `protected` members

--- Challenges/Backstage.cs
+++ Challenges/Backstage.cs
@@ -1,22 +1,57 @@
 using System;
+using System.Linq;
 using System.Reflection;
 
 namespace Challenges;
 
 public static class Backstage {
+    #region Fields
+
+    public static FieldInfo? GetFieldInfo(this Type type, string fieldName) {
+        return type.GetRuntimeFields()
+                   .SingleOrDefault(it => it.Name == fieldName);
+    }
+
+    public static FieldInfo? GetFieldInfo(this object obj, string fieldName) {
+        return obj.GetType().GetFieldInfo(fieldName);
+    }
+
     public static object? GetFieldValue(this object obj, string fieldName) {
-        return obj.GetType().GetRuntimeField(fieldName)?.GetValue(obj);
+        return obj.GetType().GetFieldInfo(fieldName)?.GetValue(obj);
+    }
+
+    #endregion
+
+    #region Properties
+
+    public static PropertyInfo? GetPropertyInfo(this Type type, string propertyName) {
+        return type.GetRuntimeProperties()
+                   .SingleOrDefault(it => it.Name == propertyName);
+    }
+
+    public static PropertyInfo? GetPropertyInfo(this object obj, string propertyName) {
+        return obj.GetType().GetPropertyInfo(propertyName);
     }
 
     public static object? GetPropertyValue(this object obj, string propertyName) {
-        return obj.GetType().GetRuntimeField(propertyName)?.GetValue(obj);
+        return obj.GetPropertyInfo(propertyName)?.GetValue(obj);
     }
 
-    public static MemberInfo? GetVariable(this Type type, string variableName) {
-        return (MemberInfo?)type.GetRuntimeProperty(variableName) ?? type.GetRuntimeField(variableName);
+    #endregion
+
+    #region "Variables" (properties if possible, otherwise, fields)
+
+    public static MemberInfo? GetVariableInfo(this Type type, string variableName) {
+        return (MemberInfo?)type.GetPropertyInfo(variableName) ?? type.GetFieldInfo(variableName);
+    }
+
+    public static MemberInfo? GetVariableInfo(this object obj, string variableName) {
+        return obj.GetType().GetVariableInfo(variableName);
     }
 
     public static object? GetVariableValue(this object obj, string variableName) {
         return obj.GetPropertyValue(variableName) ?? obj.GetFieldValue(variableName);
     }
+
+    #endregion
 }
