7fea87539c28988ac7371711e715b69a142375b0
Michael <michael.b.rawls@gmail.com>
2022-03-18 16:40:57 -0500

    Changed Item class in Challenge5 into IItem interface, added IItem to Challenge5 classes.

--- Challenges/Challenge_5/Built_In_Terfaces.cs
+++ Challenges/Challenge_5/Built_In_Terfaces.cs
@@ -1,6 +1,6 @@
 namespace Challenges.Challenge_5;
 
-public class Item : Challenge_4.Item { }
-public class BronzePickaxe : Challenge_4.BronzePickaxe { }
-public class RingOfDueling : Challenge_4.RingOfDueling { }
-public class GenieLamp : Challenge_4.GenieLamp { }
+public interface IItem { }
+public class BronzePickaxe : Challenge_4.BronzePickaxe, IItem { }
+public class RingOfDueling : Challenge_4.RingOfDueling, IItem { }
+public class GenieLamp : Challenge_4.GenieLamp, IItem { }
